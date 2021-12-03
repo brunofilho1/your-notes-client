181f1f42f273bc9cfce697beff3fbf9fc2025b50
Bruno Filho <brunowalker551@gmail.com>
2021-12-03 18:19:14 -0300

    updated modules

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const Api = axios.create({baseURL: "https://javascript-notes-api551.herokuapp.com/"});
+const Api = axios.create({baseURL: "http://localhost:3001"});
 
 export default Api;
